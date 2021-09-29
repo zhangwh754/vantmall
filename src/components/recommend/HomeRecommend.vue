623f59426c432dcb52f71baedd3d5a551e636b4b
Saiph <zhangwh754@gmail.com>
2021-09-29 13:22:20 +0800

    完成HomeRecommend组件，完成首页推荐栏

---
+++ src/components/recommend/HomeRecommend.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="recommend_box">
+    <div v-for="(recommend, index) in recommendInfo" :key="index" class="recommend_item">
+      <a :href="recommend.link">
+        <img :src="recommend.image" alt="推荐商品类">
+        <div>{{recommend.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    recommendInfo: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+  .recommend_box {
+    display: flex;
+    text-align: center;
+    font-size: 12px;
+
+    padding: 10px 0 20px;
+    border-bottom: 8px solid #eee;
+  }
+  .recommend_item {
+    flex: 1;
+  }
+
+  .recommend_item img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <home-nav-bar></home-nav-bar>
     <home-swipe :images="banners"></home-swipe>
+    <home-recommend :recommendInfo="recommends"></home-recommend>
     <h2>首页</h2>
   </div>
 </template>
@@ -10,12 +11,14 @@ import { getHomeMultidata } from 'network/home.js'
 
 import HomeNavBar from 'components/navbar/HomeNavBar.vue'
 import HomeSwipe from 'components/swipe/HomeSwipe.vue'
+import HomeRecommend from 'components/recommend/HomeRecommend.vue'
 
 export default {
   name: 'home',
   components: {
     HomeNavBar,
-    HomeSwipe
+    HomeSwipe,
+    HomeRecommend
   },
   data() {
     return {
