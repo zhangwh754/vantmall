f13ea00b133666c2ba18d4b4266c5c7da199b0ae
Saiph <zhangwh754@gmail.com>
2021-09-29 14:03:25 +0800

    完成homefeatureview组件，完成首页视图选项卡

---
+++ src/components/featureview/HomeFeatureView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home_feature_view">
+    <a href="www.baidu.com">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="featureview">
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'homefeatureview'
+}
+</script>
+<style>
+  .home_feature_view img {
+    width: 100%;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <home-nav-bar></home-nav-bar>
     <home-swipe :images="banners"></home-swipe>
     <home-recommend :recommendInfo="recommends"></home-recommend>
+    <home-feature-view></home-feature-view>
     <h2>首页</h2>
   </div>
 </template>
@@ -12,13 +13,15 @@ import { getHomeMultidata } from 'network/home.js'
 import HomeNavBar from 'components/navbar/HomeNavBar.vue'
 import HomeSwipe from 'components/swipe/HomeSwipe.vue'
 import HomeRecommend from 'components/recommend/HomeRecommend.vue'
+import HomeFeatureView from 'components/featureview/HomeFeatureView.vue'
 
 export default {
   name: 'home',
   components: {
     HomeNavBar,
     HomeSwipe,
-    HomeRecommend
+    HomeRecommend,
+    HomeFeatureView
   },
   data() {
     return {
