34d2795b66bc02ff630ce3379ab3c94e19c6b1ec
Saiph <zhangwh754@gmail.com>
2021-09-29 10:17:01 +0800

    导入网络请求request.js

---
+++ src/network/request.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export function request(config) {
+  const instance = axios.create({
+    baseURL: 'http://152.136.185.210:7878/api/hy66',
+    timeout: 5000
+  })
+  instance.interceptors.request.use(res => {
+    console.log('request拦截器', res);
+    return res
+  })
+
+  instance.interceptors.response.use(config => {
+    console.log('response拦截器', config.data);
+    return config.data
+  }, err => {
+    console.log(err);
+  })
+
+  return instance(config)
+}
