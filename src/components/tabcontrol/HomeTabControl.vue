028d9a8841968ab5ad014f74cea85ef5d91cbd9d
Saiph <zhangwh754@gmail.com>
2021-09-29 21:04:07 +0800

    用List插件完成下拉获取对应新数据

--- src/components/tabcontrol/HomeTabControl.vue
+++ src/components/tabcontrol/HomeTabControl.vue
@@ -2,10 +2,15 @@
   <div class="tabcontrol">
     <van-tabs sticky offset-top="46px" title-active-color="var(--color-tint)">
       <van-tab v-for="(item, index) in tab" :key="index" :title="item.name">
-        <div>
+        <van-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad(item['type'])"
+        >
           <h2 v-for="(good, index) in goods[item['type']].list" :key="index">{{item['type']}}：{{index+1}}、{{good.price}}</h2>
-          <button @click="btnClick(item['type'])">加载新数据</button>
-        </div>
+        </van-list>
+        <!-- <button @click="btnClick(item['type'])">加载新数据</button> -->
       </van-tab>
     </van-tabs>
   </div>
@@ -13,10 +18,17 @@
 <script>
 export default {
   name: 'hometabcontrol',
+  data() {
+    return {
+      loading: false,
+      finished: false,
+    }
+  },
   methods: {
-    btnClick(type) {
+    onLoad(type) {
       console.log(type);
       this.$emit('loadNewGood', type)
+      this.loading = false;
     }
   },
   props: {
