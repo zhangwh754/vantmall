2641f9e806430f54e331439f10132ce558715bee
Saiph <zhangwh754@gmail.com>
2021-09-30 18:54:09 +0800

    tabbar切换为vant tab组件自带的to路由跳转

--- src/components/tabbar/TabBar.vue
+++ src/components/tabbar/TabBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <van-tabbar v-model="active" active-color="#ee0a24" inactive-color="#000" placeholder>
-      <van-tabbar-item icon="home-o" @click="homeClick">主页</van-tabbar-item>
-      <van-tabbar-item icon="filter-o" @click="categoriesClick">分类</van-tabbar-item>
-      <van-tabbar-item icon="cart-o" @click="cartClick">购物车</van-tabbar-item>
-      <van-tabbar-item icon="contact" @click="profileClick">我的</van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/home">主页</van-tabbar-item>
+      <van-tabbar-item icon="filter-o" to="/category">分类</van-tabbar-item>
+      <van-tabbar-item icon="cart-o" to="/cart">购物车</van-tabbar-item>
+      <van-tabbar-item icon="contact" to="/profile">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
@@ -15,20 +15,6 @@ export default {
     return {
       active: 0,
     };
-  },
-  methods: {
-    homeClick() {
-      this.$router.push('/home')
-    },
-    categoriesClick() {
-      this.$router.push('/category')
-    },
-    cartClick() {
-      this.$router.push('/cart')
-    },
-    profileClick() {
-      this.$router.push('/profile')
-    }
   }
 }
 </script>
