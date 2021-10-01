323a4227f06bb5fd257c60b41292e02d3c4eae10
Saiph <zhangwh754@gmail.com>
2021-10-01 16:35:18 +0800

    时间戳转换的工具方法

---
+++ src/common/utils.js
@@ -0,0 +1,24 @@
+//自动格式化时间戳的函数，传入时间戳和需要格式化的格式
+export function formatDate(date, fmt) {
+  if (typeof date == 'string') {
+    return date;
+  }
+
+  if (!fmt) fmt = "yyyy-MM-dd hh:mm:ss";
+
+  if (!date || date == null) return null;
+  var o = {
+    'M+': date.getMonth() + 1, // 月份
+    'd+': date.getDate(), // 日
+    'h+': date.getHours(), // 小时
+    'm+': date.getMinutes(), // 分
+    's+': date.getSeconds(), // 秒
+    'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+    'S': date.getMilliseconds() // 毫秒
+  }
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+  }
+  return fmt
+}
